8c5a52bd76423e14ed980482e18b7ac6448b1405
I-like-pelmeni <maxtoo2017@gmail.com>
2022-06-07 18:40:36 +0300

    Add win text

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ bot = telebot.TeleBot(bot_token)
 
 DIGITS = [str(x) for x in range(10)]
 my_number = ''
+active_game = False
 
 @bot.message_handler(commands=['start', 'game'])
 def start_game(message):
     digits = DIGITS.copy()
-    global my_number
+    global my_number, active_game
     my_number = ''
     for pos in range(4):
         if pos:
@@ -19,6 +20,7 @@ def start_game(message):
             digit = random.choice(digits[1:])
         my_number += digit
         digits.remove(digit)
+    active_game = True
     bot.reply_to(message, 'Игра в быки и коровы. '
         f'Я загадал 4-значное число. Попробуй отгадать, {message.from_user.first_name}!')
 
@@ -32,6 +34,7 @@ def show_help(message):
 
 @bot.message_handler(content_types=['text'])
 def bot_answer(message):
+    global active_game
     text = message.text
     if len(text) == 4 and text.isnumeric() and len(text) == len(set(text)):
         cows, bulls = 0, 0
@@ -42,12 +45,22 @@ def bot_answer(message):
                 else:
                     cows += 1
         if bulls == 4:
-            response = 'Ты выиграл!'
+            active_game = False
+            response = 'Ты выиграл! Сыграем ещё?'
+            bot.send_message(message.from_user.id, response, reply_markup=get_buttons())
+            return
         else:
             response = f'🐂bulls: {bulls} / 🐄cows: {cows}'
     else:
         response = 'Пришли мне 4-значное число с разными цыфрами!'
     bot.send_message(message.from_user.id, response)
+
+def get_buttons():
+    buttons = telebot.types.ReplyKeyboardMarkup( 
+        one_time_keyboard=True, 
+        resize_keyboard=True
+)
+    buttons.add('Да', 'Нет')
 
 if __name__ == '__main__':
     bot.polling(non_stop=True)
