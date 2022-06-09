4a5b117adf25c8362e9107ab709c6be0133e3685
I-like-pelmeni <maxtoo2017@gmail.com>
2022-06-09 18:55:33 +0300

    Add levels

--- main.py
+++ main.py
@@ -9,10 +9,16 @@ bot = telebot.TeleBot(bot_token)
 DIGITS = [str(x) for x in range(10)]
 
 @bot.message_handler(commands=['start', 'game'])
-def start_game(message):
+def select_level(message):
+    response = 'Игра "Быки и коровы"\n' + \
+               'Выбери уровень (количество цифр)'
+    bot.send_message(message.from_user.id, response,
+        reply_markup=get_level())    
+
+def start_game(message, level):
     digits = DIGITS.copy()
     my_number = ''
-    for pos in range(4):
+    for pos in range(level):
         if pos:
             digit = random.choice(digits)
         else: 
@@ -23,7 +29,7 @@ def start_game(message):
     with shelve.open(db_name) as storage:
         storage[str(message.from_user.id)] = my_number
     bot.reply_to(message, 'Игра "Быки и коровы"\n'
-        f'Я загадал 4-значное число. Попробуй отгадать, {message.from_user.first_name}!')
+        f'Я загадал {level}-значное число. Попробуй отгадать, {message.from_user.first_name}!')
 
 @bot.message_handler(commands=['help'])
 def show_help(message):
@@ -34,36 +40,39 @@ def show_help(message):
 
 @bot.message_handler(content_types=['text'])
 def bot_answer(message):
-    global active_game
     text = message.text
     try:
         with shelve.open(db_name) as storage:
             my_number = storage[str(message.from_user.id)]
-        if len(text) == 4 and text.isnumeric() and len(text) == len(set(text)):
+        level = len(my_number)
+        if len(text) == level and text.isnumeric() and len(text) == len(set(text)):
             cows, bulls = 0, 0
-            for i in range(4):
+            for i in range(level):
                 if text[i] in my_number:
                     if text[i] == my_number[i]:
                         bulls += 1
                     else:
                         cows += 1
-            if bulls == 4:
-                print(f'{my_number} was discovered by {message.from_user.username}!')
+            if bulls == level:
+                print(f'{my_number} was discovered by {message.from_user.username} !')
                 with shelve.open(db_name) as storage:
                     del storage[str(message.from_user.id)]
                 response = 'Ты угадал! Сыграем еще?\n\n' + \
-                            '_Приходи учиться в КИТ создавать ботов для Telegram_\n' + \
-                            'https://kit.kh.ua'
+                           '_Приходи учиться в Кит создавать ботов для Telegram_\n' + \
+                           'https://kit.kh.ua/'
                 bot.send_message(message.from_user.id, response,
-                    reply_markup=get_buttons(), parse_mode = 'Markdown')
+                    reply_markup=get_buttons(), parse_mode='Markdown')
                 return
             else:
                 response = f'Быки: {bulls} | Коровы : {cows}'
         else:
-            response = 'Пришли мне 4-значное число с разными цифрами!'
+            response = f'Пришли мне {level}-значное число с разными цифрами!'
     except KeyError:
-        if text == 'Да':
-            start_game(message)
+        if text in ('3', '4', '5'):
+            start_game(message, int(text))
+            return
+        elif text == 'Да':
+            select_level(message)
             return
         else:
             response = 'Для запуска игры набери /start'
@@ -75,6 +84,14 @@ def get_buttons():
         resize_keyboard=True
     )
     buttons.add('Да', 'Нет')
+    return buttons
+
+def get_level():
+    buttons = telebot.types.ReplyKeyboardMarkup(
+        one_time_keyboard=True,
+        resize_keyboard=True
+    )
+    buttons.add('3', '4', '5')
     return buttons
 
 if __name__ == '__main__':
