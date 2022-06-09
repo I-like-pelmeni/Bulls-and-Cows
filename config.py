36a4af632c2ea1d1e1a362bf7e62c30b3eb286d8
I-like-pelmeni <maxtoo2017@gmail.com>
2022-06-09 18:14:36 +0300

    Update code

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 bot_token = '5405530966:AAHA5QqpEXXBdRLrQTXIOL8JBz5U3V2qoxA'
+db_name = 'bot.db'
--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import telebot
 import random
-from config import bot_token
+import shelve
+
+from config import bot_token, db_name
 
 bot = telebot.TeleBot(bot_token)
 
 DIGITS = [str(x) for x in range(10)]
-my_number = ''
-active_game = False
 
 @bot.message_handler(commands=['start', 'game'])
 def start_game(message):
     digits = DIGITS.copy()
-    global my_number, active_game
     my_number = ''
     for pos in range(4):
         if pos:
@@ -20,52 +19,59 @@ def start_game(message):
             digit = random.choice(digits[1:])
         my_number += digit
         digits.remove(digit)
-    active_game = True
-    bot.reply_to(message, 'Игра в быки и коровы. '
+    print(f'{my_number} for {message.from_user.username}')
+    with shelve.open(db_name) as storage:
+        storage[str(message.from_user.id)] = my_number
+    bot.reply_to(message, 'Игра "Быки и коровы"\n'
         f'Я загадал 4-значное число. Попробуй отгадать, {message.from_user.first_name}!')
 
 @bot.message_handler(commands=['help'])
 def show_help(message):
     bot.reply_to(message, """
 Игра Быки и коровы
-
-Игра, в которой игрок должен определить число задуманое ботом как можно быстрее, при каждой попытке бот сообщает сколько чисел ты угадал(количество коров) и какие из них стоят правильно(количество быков).
-    """)
+Игра, в ходе которой за несколько попыток игрок должен определить 4-значное число, задуманное ботом. После каждой попытки бот сообщает количество угаданных цифр без с их позициями (количество «коров») и полных совпадений (количество «быков»).
+""")
 
 @bot.message_handler(content_types=['text'])
 def bot_answer(message):
     global active_game
     text = message.text
-    if not active_game:
+    try:
+        with shelve.open(db_name) as storage:
+            my_number = storage[str(message.from_user.id)]
+        if len(text) == 4 and text.isnumeric() and len(text) == len(set(text)):
+            cows, bulls = 0, 0
+            for i in range(4):
+                if text[i] in my_number:
+                    if text[i] == my_number[i]:
+                        bulls += 1
+                    else:
+                        cows += 1
+            if bulls == 4:
+                print(f'{my_number} was discovered by {message.from_user.username}!')
+                with shelve.open(db_name) as storage:
+                    del storage[str(message.from_user.id)]
+                response = 'Ты угадал! Сыграем еще?'
+                bot.send_message(message.from_user.id, response,
+                    reply_markup=get_buttons())
+                return
+            else:
+                response = f'Быки: {bulls} | Коровы : {cows}'
+        else:
+            response = 'Пришли мне 4-значное число с разными цифрами!'
+    except KeyError:
         if text == 'Да':
             start_game(message)
             return
         else:
-            response = 'До запуска игры набери /start'
-    elif len(text) == 4 and text.isnumeric() and len(text) == len(set(text)):
-        cows, bulls = 0, 0
-        for i in range(4):
-            if text[i] in my_number:
-                if text[i] == my_number[i]:
-                    bulls += 1
-                else:
-                    cows += 1
-        if bulls == 4:
-            active_game = False
-            response = 'Ты выиграл! Сыграем ещё?'
-            bot.send_message(message.from_user.id, response, reply_markup=get_buttons())
-            return
-        else:
-            response = f'🐂bulls: {bulls} / 🐄cows: {cows}'
-    else:
-        response = 'Пришли мне 4-значное число с разными цыфрами!'
+            response = 'Для запуска игры набери /start'
     bot.send_message(message.from_user.id, response)
 
 def get_buttons():
-    buttons = telebot.types.ReplyKeyboardMarkup( 
-        one_time_keyboard=True, 
+    buttons = telebot.types.ReplyKeyboardMarkup(
+        one_time_keyboard=True,
         resize_keyboard=True
-)
+    )
     buttons.add('Да', 'Нет')
     return buttons
 
